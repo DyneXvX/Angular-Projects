9f08e38455cc7d9673cfa732bd1b44e1237292f1
Justin Thoms <DyneXvX@outlook.com>
2021-05-28 13:17:26 -0400

    Section 5 Complete with Assignment Done

--- Complete-Guide/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ Complete-Guide/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -7,9 +7,10 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 })
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
-  @Output() blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
-  newServerName = '';
-  newServerContent = '';
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  //newServerName = '';
+  //newServerContent = '';
+  @ViewChild('serverContentInput', {static:true}) serverContentInput : ElementRef;
 
   constructor() {
   }
@@ -17,17 +18,18 @@ export class CockpitComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onAddServer() {
+  onAddServer(serverNameInput: HTMLInputElement) {
+
     this.serverCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent
-    });
+      serverName: serverNameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
+   });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(serverNameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent
+      serverName: serverNameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
--- Complete-Guide/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ Complete-Guide/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,15 +1,33 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterContentInit, Component, DoCheck, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
+
 })
-export class ServerElementComponent implements OnInit {
-  @Input('srvElement') element: {type: string, name: string, content: string};
-  constructor() { }
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit {
+  @Input('srvElement') element: { type: string, name: string, content: string };
+
+  constructor() {
+    console.log('constructor called!')
+  }
+
+  ngOnChanges(changes:SimpleChanges){
+    console.log('ngOnChanges called!')
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit called!')
+  }
+
+  ngDoCheck(){
+    console.log('ngDoCheck Called!')
+  }
+
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit Called!')
   }
 
 }
