03cf8a714cb53ac88c29cdc1705e8182d4e8649f
Justin Thoms <justin.thoms@roirocket.com>
2021-06-01 13:56:04 -0400

    Section 13 - Understanding Observables

---
+++ Complete-Guide/obs-01-start/src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription, interval} from 'rxjs';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstObsSub: Subscription;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.firstObsSub = interval(1000).subscribe(count => {
+      console.log(count);
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.firstObsSub.unsubscribe();
+  }
+}
